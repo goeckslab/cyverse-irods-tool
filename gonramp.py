43f9893d57b6bf30784f6585d3036dbec7baaf97
Luke Sargent <luke.c.sargent@gmail.com>
2018-06-10 15:30:02 -0700

    ayy

--- gonramp.py
+++ gonramp.py
@@ -14,7 +14,7 @@ ap.add_argument("--password")
 ap.add_argument("--timestamp", action="store_true")
 ap.add_argument("--output")
 
-anon_prefix = "https://de.cyverse.org/anon-files/"
+anon_prefix = "https://de.cyverse.org/anon-files/iplant/home/"
 
 args = ap.parse_args()
 
@@ -27,11 +27,10 @@ if args.user and args.password:
 conn = CyVerseiRODS(**kwargs)
 
 default_perm = {
-  "type" : "write",
+  "type" : "read",
   "name" : "anonymous",
   "zone" : conn.KWARGS["zone"]
 }
-
 
 # generate timestamp, if relevant
 if args.timestamp or not args.remotedestination:
@@ -45,55 +44,56 @@ remote_subdir = args.remotedestination
 
 args.remotedestination = conn.user_dir + "/" + args.remotedestination
 
+# UCSC has a "hub.txt"
+# JBrowse has a "myHub/trackList.json"
+ucsc_specific = remote_subdir + "/hub.txt"
+jbrowse_specific = remote_subdir
+data_url = anon_prefix + kwargs["user"] + "/"
+hubtype=""
+title=""
+header="Link to generated {} hub:"
+
+# find where the files REALLY live
+args.localsource = ".".join(args.localsource.split('.')[:-1]) + "_files/"
+
+# ensure we have a valid archive
+if path.isfile(args.localsource + "hub.txt"):
+    data_url = data_url + ucsc_specific
+    hubtype="UCSC"
+    url="http://genome.ucsc.edu/cgi-bin/hgHubConnect?hgHub_do_redirect=on&hgHubConnect.remakeTrackHub=on&hgHub_do_firstDb=1&hubClear={}"
+elif path.isfile(args.localsource + "myHub/trackList.json"):
+    args.localsource = args.localsource + "myHub/"
+    data_url = data_url + jbrowse_specific
+    url="https://de.cyverse.org/anon-files/iplant/home/shared/G-OnRamp_hubs/JBrowse-1.12.3/index.html?data={}"
+    hubtype = "JBrowse"
+else:
+    raise OSError("Neither '{}' nor '{}' found.".format(args.localsource + "hub.txt", args.localsource + "myHub/trackList.json"))
+
 # upload
 if args.upload:
     if args.localsource is None:
         parser.error("--upload requires --localsource, --remotedestination is optional")
     else:
-        args.localsource = ".".join(args.localsource.split('.')[:-1]) + "_files/"
-        print("this is where i'd run: conn.recursive_upload(args.localsource, args.remotedestination, default_perm)\n{}\n{}\n{}\n\n".format(args.localsource, args.remotedestination, default_perm))
-        exit()
+        print("conn.recursive_upload(args.localsource, args.remotedestination, default_perm)\n{}\n{}\n{}\n\n".format(args.localsource, args.remotedestination, default_perm))
         conn.recursive_upload(args.localsource, args.remotedestination, default_perm)
 
-# UCSC has a "hub.txt"
-# JBrowse has a "json/trackList.json"
-ucsc_specific = remote_subdir + "/hub.txt"
-jbrowse_specific = remote_subdir + "/json"
-local = ""
-data_url = anon_prefix + kwargs["user"] + "/"
-hubtype=None
-title=""
-header="Link to generated {} hub:"
-
-if path.isfile(ucsc_specific):
-    data_url = data_url + ucsc_specific
-    hubtype="UCSC"
-    url="http://genome.ucsc.edu/cgi-bin/hgHubConnect?hgHub_do_redirect=on&hgHubConnect.remakeTrackHub=on&hgHub_do_firstDb=1&hubClear={}"
-elif path.isfile(jbrowse_specific):
-    data_url = data_url + jbrowse_specific
-    url="https://de.cyverse.org/anon-files/iplant/home/shared/G-OnRamp_hubs/JBrowse-1.12.3/index.html?data={}"
-    hubtype = "JBrowse"
-else:
-    raise OSError("Neither '{}' nor '{}' found.".format(ucsc_specific, jbrowse_specific))
-
-header.format(hubtype)
-url.format(data_url)
+header = header.format(hubtype)
+url = url.format(data_url)
 
 # generate link
-html_content = '''
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>{}</title>
-  </head>
-  <body>
-    <h1>{}</h1>
-    <a href="{}">Generated Hub at {}</a>
-  </body>
-</html>
-'''.format(title, header, url, kwargs["user"] + "/" + remote_subdir)
+html_content = str('''
+<!DOCTYPE html>\
+<html lang="en">\
+  <head>\
+    <title>{}</title>\
+  </head>\
+  <body>\
+    <h1>{}</h1>\
+    <a href="{}">Generated Hub at {}</a>\
+  </body>\
+</html>''')
 
 # generate HTML file
 if args.output:
     with open(args.output, "w") as file:
-        file.write(html_content)
+        file.write(html_content.format(title, header, url, kwargs["user"] + "/" + remote_subdir))
