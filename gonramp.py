bf3f7cc83212dc25bdcd7130b1c53eb12517cc81
luke-c-sargent <luke.c.sargent@gmail.com>
2018-06-09 20:06:54 -0700

    changes to dependency mgmt for python-irodsclient

--- gonramp.py
+++ gonramp.py
@@ -12,7 +12,9 @@ ap.add_argument("--user")
 ap.add_argument("--password")
 ap.add_argument("--timestamp", action="store_true")
 
-data_c_path = "/iplant/home/shared/G-OnRamp_hubs/community/"
+anon_prefix = "https://de.cyverse.org/anon-files/"
+
+
 
 args = ap.parse_args()
 
@@ -23,6 +25,13 @@ if args.user and args.password:
 
 # initialize connection
 conn = CyVerseiRODS(**kwargs)
+
+default_perm = {
+  "type" : "write",
+  "name" : "anonymous",
+  "zone" : CyRODS.KWARGS["zone"]
+}
+
 
 # generate timestamp, if relevant
 if args.timestamp or not args.remotedestination:
@@ -40,7 +49,7 @@ if args.upload:
         parser.error("--upload requires --localsource, --remotedestination is optional")
     else:
         args.remotedestination = conn.user_dir if not args.remotedestination else args.remotedestination
-        conn.recursive_upload(args.localsource, args.remotedestination)
+        conn.recursive_upload(args.localsource, args.remotedestination, default_perm)
 
 # this is the location of the file to write the url to
 if args.output:
@@ -52,18 +61,38 @@ dataset_dir = args.remotedestination.split(',')[0] + "_files/"
 # UCSC has a "hub.txt"
 # JBrowse has a "json/trackList.json"
 ucsc_specific = dataset_dir + "hub.txt"
-jbrowse_specific = dataset_dir + "json/trackList.json"
+jbrowse_specific = dataset_dir + "json"
 local = ""
+url = ""
 hubtype=None
 if path.isfile(ucsc_specific):
     local = ucsc_specific
     hubtype="UCSC"
+    url="http://genome.ucsc.edu/cgi-bin/hgHubConnect?hgHub_do_redirect=on&hgHubConnect.remakeTrackHub=on&hgHub_do_firstDb=1&hubClear={}"
 elif path.isfile(jbrowse_specific):
     local = jbrowse_specific
+    url="https://de.cyverse.org/anon-files/iplant/home/shared/G-OnRamp_hubs/JBrowse-1.12.3/index.html?data={}"
     hubtype = "JBROWSE"
 else:
     raise OSError("Neither '{}' nor '{}' found.".format(ucsc_specific, jbrowse_specific))
 
+url.format(data_url)
+
 # generate link
+html = '''
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>{}</title>
+  </head>
+  <body>
+    <h1>{}</h1>
+    <a href="{}">Generated Hub</a>
+  </body>
+</html>
+'''.format(title, header, url)
+
+
+
 
 # generate HTML file
